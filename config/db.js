7b3b51e76acc7fc9edb02e158de543b1d7c07496
PrashantAnand03 <prashantanandcse2025@gmail.com>
2025-02-11 02:08:32 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        // 'mongodb://127.0.0.1:27017/internship'
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000, // Increase timeout
+            socketTimeoutMS: 45000, // Longer socket timeout
+            keepAlive: true,
+            keepAliveInitialDelay: 300000,
+        });
         console.log('MongoDB connected');
     } catch (error) {
         console.error(`MongoDB connection error: ${process.env.MONGO_URI}`, error.message);
