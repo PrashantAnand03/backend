6958cdd51923542b05a5351945dc4c3b7e2b9db6
PrashantAnand03 <prashantanandcse2025@gmail.com>
2025-02-11 02:10:56 +0530

    ch

--- config/db.js
+++ config/db.js
@@ -7,12 +7,10 @@ const connectDB = async () => {
             useUnifiedTopology: true,
             serverSelectionTimeoutMS: 5000, // Increase timeout
             socketTimeoutMS: 45000, // Longer socket timeout
-            keepAlive: true,
-            keepAliveInitialDelay: 300000,
         });
         console.log('MongoDB connected');
     } catch (error) {
-        console.error(`MongoDB connection error: ${process.env.MONGO_URI}`, error.message);
+        console.error(`MongoDB connection error: ${error.message}`);
         process.exit(1);
     }
 };
